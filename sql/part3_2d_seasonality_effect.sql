e586f344638fdddbb4887bcc79879eda0b3a25b3
Joseph Griffin <93599297+joegriff19@users.noreply.github.com>
2025-11-04 17:46:00 +0100

    Update header

--- sql/part3_2d_seasonality_effect.sql
+++ sql/part3_2d_seasonality_effect.sql
@@ -1,3 +1,6 @@
+-- part3_2d_seasonality_effect.sql
+-- Purpose: Evaluate correlation between seasonality index and revenue to measure cyclical effects.
+
 SELECT
   AVG(ef.seasonality_index) AS avg_seasonality_index,
   AVG(r.revenue) AS avg_revenue,
